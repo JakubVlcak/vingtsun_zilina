0c0bd26c60bc7519d025a7b3bb98eb2a918b6980
JV <jakub.vlcak888@gmail.com>
2026-04-02 20:10:21 +0200

    Switch to HashRouter for static hosting compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './views/Home';
+import Clanky from './views/Clanky';
+import ClanokDetail from './views/ClanokDetail';
 
 function App() {
   return (
-    <>
+    <HashRouter>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/clanky" element={<Clanky />} />
+        <Route path="/clanky/:slug" element={<ClanokDetail />} />
+      </Routes>
       <Footer />
-    </>
+    </HashRouter>
   );
 }
 
